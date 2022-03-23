985ac08ac213c7c6eb4adfc3def7c715b40249f3
yussuf <yosef.awad.1@gmail.com>
2022-03-23 21:30:33 +0200

    fineshed home desighn and animations

---
+++ script/script.js
@@ -0,0 +1,27 @@
+//fading animation start
+const homeAnimatedText = document.querySelector(".changingHeader");
+const strHomeAnimatedText = homeAnimatedText.textContent;
+const splitHomeAnimatedText = strHomeAnimatedText.split("");
+homeAnimatedText.textContent = "";
+for (let i = 0; i < splitHomeAnimatedText.length; i++) {
+    homeAnimatedText.innerHTML += "<span>" + splitHomeAnimatedText[i] + "</span>";
+}
+
+var charIndex = 0;
+var timer = setInterval(onAnimationTick, 50);
+
+function onAnimationTick() {
+    const span = homeAnimatedText.querySelectorAll("span")[charIndex];
+    span.classList.add("fadeAnimation");
+    charIndex++;
+    if (charIndex === splitHomeAnimatedText.length) {
+        completeAnimation();
+        return;
+    }
+}
+
+function completeAnimation() {
+    clearInterval(timer);
+    timer = null;
+}
+//fading animation end
